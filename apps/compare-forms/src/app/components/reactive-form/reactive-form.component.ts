75ba7ea5017645571ec47bc7c6840bad16bb80ac
xocomil <xocomil@gmail.com>
2022-10-01 15:03:07 -0600

    added buttons and code to handle buttons

--- apps/compare-forms/src/app/components/reactive-form/reactive-form.component.ts
+++ apps/compare-forms/src/app/components/reactive-form/reactive-form.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { PushModule } from '@ngrx/component';
+import { EmailFormModel, emptyEmailForm } from '../../models/email-form.model';
 import { EmailFrequency } from '../../models/email-frequency';
 import { ObjectToEntriesPipe } from '../template-driven/object-to-entries.pipe';
 
@@ -19,6 +21,7 @@ import { ObjectToEntriesPipe } from '../template-driven/object-to-entries.pipe';
     PushModule,
     ReactiveFormsModule,
     ObjectToEntriesPipe,
+    MatButtonModule,
   ],
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.scss'],
@@ -33,4 +36,39 @@ export class ReactiveFormComponent {
   protected emailFrequencies = EmailFrequency;
 
   protected readonly formValues$ = this.formGroup.valueChanges;
+
+  resetForm(): void {
+    this.formGroup.reset(emptyEmailForm());
+  }
+
+  saveForm(): void {
+    this.#reallySaveTheForm(this.#transformToEmailFormModel(this.formGroup));
+  }
+
+  #reallySaveTheForm(model: EmailFormModel) {
+    console.log('Reactive form saved!', model);
+
+    window.alert(
+      `Reactive form was saved with value: ${JSON.stringify(model)}`
+    );
+
+    this.resetForm();
+  }
+
+  #transformToEmailFormModel(form: typeof this.formGroup): EmailFormModel {
+    const { emailFrequency, email, agreedToEmails } = form.value;
+
+    if (emailFrequency) {
+      return {
+        email: email ?? '',
+        agreedToEmails: agreedToEmails ?? false,
+        emailFrequency,
+      };
+    }
+
+    return {
+      email: email ?? '',
+      agreedToEmails: agreedToEmails ?? false,
+    };
+  }
 }
--- apps/compare-forms/src/app/components/template-driven/template-driven.component.ts
+++ apps/compare-forms/src/app/components/template-driven/template-driven.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { EmailFormModel } from '../../models/email-form.model';
+import { EmailFormModel, emptyEmailForm } from '../../models/email-form.model';
 import { EmailFrequency } from '../../models/email-frequency';
 import { ObjectToEntriesPipe } from './object-to-entries.pipe';
 
@@ -20,15 +21,27 @@ import { ObjectToEntriesPipe } from './object-to-entries.pipe';
     FormsModule,
     MatSelectModule,
     ObjectToEntriesPipe,
+    MatButtonModule,
   ],
   templateUrl: './template-driven.component.html',
   styleUrls: ['./template-driven.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
 })
 export class TemplateDrivenComponent {
-  protected vm: EmailFormModel = {
-    email: '',
-    agreedToEmails: false,
-  };
+  protected vm: EmailFormModel = emptyEmailForm();
   protected emailFrequencies = EmailFrequency;
+
+  resetForm(): void {
+    this.vm = emptyEmailForm();
+  }
+
+  saveForm(): void {
+    console.log('Template form saved!', this.vm);
+
+    window.alert(
+      `Template form was saved with value: ${JSON.stringify(this.vm)}`
+    );
+
+    this.resetForm();
+  }
 }
