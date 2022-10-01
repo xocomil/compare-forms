37278e6297b33fa756d6cb9d18740365cd82d7c5
xocomil <xocomil@gmail.com>
2022-10-01 15:34:12 -0600

    add required for email

--- apps/compare-forms/src/app/components/template-driven/template-driven.component.ts
+++ apps/compare-forms/src/app/components/template-driven/template-driven.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewEncapsulation } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -30,12 +30,23 @@ import { ObjectToEntriesPipe } from './object-to-entries.pipe';
 export class TemplateDrivenComponent {
   protected vm: EmailFormModel = emptyEmailForm();
   protected emailFrequencies = EmailFrequency;
+  @ViewChild('form', { static: true }) form!: NgForm;
 
   resetForm(): void {
+    this.form.resetForm();
+
     this.vm = emptyEmailForm();
   }
 
-  saveForm(): void {
+  saveForm(): false | void {
+    if (!this.form.valid) {
+      console.log('form', this.form);
+
+      alert(`There are issues with your form. Please fix them and try again.`);
+
+      return false;
+    }
+
     console.log('Template form saved!', this.vm);
 
     window.alert(
