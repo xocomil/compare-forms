8645d4c6e649d1d57fed9ee5ffad4e9d36ce3231
xocomil <xocomil@gmail.com>
2022-10-08 17:06:07 -0600

    all form validation working

--- apps/compare-forms/src/app/components/template-driven/template-driven.component.ts
+++ apps/compare-forms/src/app/components/template-driven/template-driven.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { Subscription } from 'rxjs';
 import { EmailFormModel, emptyEmailForm } from '../../models/email-form.model';
 import { EmailFrequency } from '../../models/email-frequency';
 import { ObjectToEntriesPipe } from './object-to-entries.pipe';
@@ -27,10 +34,26 @@ import { ObjectToEntriesPipe } from './object-to-entries.pipe';
   styleUrls: ['./template-driven.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class TemplateDrivenComponent {
+export class TemplateDrivenComponent implements OnInit, OnDestroy {
   protected vm: EmailFormModel = emptyEmailForm();
   protected emailFrequencies = EmailFrequency;
   @ViewChild('form', { static: true }) form!: NgForm;
+
+  readonly #subs = new Subscription();
+
+  ngOnInit(): void {
+    this.#subs.add(
+      this.form.valueChanges?.subscribe((changes: EmailFormModel) => {
+        if (!changes.agreedToEmails) {
+          this.vm.emailFrequency = undefined;
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.#subs.unsubscribe();
+  }
 
   resetForm(): void {
     this.form.resetForm();
